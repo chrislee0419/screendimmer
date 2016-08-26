fb09c1b0037a34de12b0c73bd027990d78f37ab5
chrislee0419 <cla235@sfu.ca>
2016-08-26 16:24:39 -0700

    initial commit

---
+++ ScreenDimmer/OptionsForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ScreenDimmer
+{
+    public class OptionsForm : Form
+    {
+        // constructor
+        public OptionsForm()
+        {
+            // set form attributes
+
+
+            InitializeTabs();
+        }
+
+        // create tabs for individual screens
+        private void InitializeTabs()
+        {
+
+        }
+    }
+}
---
+++ ScreenDimmer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ScreenDimmer
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            // form setup
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // check for existing .xml file
+            // if one exists, send information to form constructor
+            // if one does not exist, use default form constructor
+            try
+            {
+                XDocument xml_doc = XDocument.Load("settings.xml");
+                var screens = xml_doc.Descendants("Screen");
+            }
+            catch (FileNotFoundException)
+            {
+                Application.Run(new OptionsForm());
+            }
+        }
+    }
+}
